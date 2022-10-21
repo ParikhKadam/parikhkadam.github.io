3b4f1a065327c94294b46d4ff94c46f24135993a
Kadam Parikh <parikhkadamgmail.com>
2022-10-22 01:00:26 +0530

    open links in new tab

--- _next/static/Yk_TSNcHm2TQjF9qThlgb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/425-e280ff72f07164ea.js","static/chunks/pages/index-64a8cc76425001ab.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
---
+++ _next/static/az1J85ffIpWVW2ZbwuSkV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/425-e280ff72f07164ea.js","static/chunks/pages/index-0a6b0f1338cfd8b3.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
