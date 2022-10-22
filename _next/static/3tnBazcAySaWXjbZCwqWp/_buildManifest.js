c9067b04b8d7a8d251b75d0e3e1371a06cfeb706
Kadam Parikh <parikhkadamgmail.com>
2022-10-22 19:01:42 +0530

    added link preview meta tags

---
+++ _next/static/3tnBazcAySaWXjbZCwqWp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/425-e280ff72f07164ea.js","static/chunks/pages/index-89eba7e003c8c3c8.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
