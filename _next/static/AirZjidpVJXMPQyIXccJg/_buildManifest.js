26c9e31972903e0453252307f3c00d8d7164658e
Kadam Parikh <parikhkadamgmail.com>
2022-10-22 16:00:09 +0530

    updated profilepic, logo; added mailto link for contact form

---
+++ _next/static/AirZjidpVJXMPQyIXccJg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/425-e280ff72f07164ea.js","static/chunks/pages/index-b79cb53817a1635b.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
