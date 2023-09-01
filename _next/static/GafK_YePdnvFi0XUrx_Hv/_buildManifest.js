de86af104237feb74696cf16fc15badb4528da52
Kadam Parikh <parikhkadam@gmail.com>
2023-09-01 23:29:45 +0530

    seo related changes

--- _next/static/GafK_YePdnvFi0XUrx_Hv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/118-3ce137c3ada2ee62.js","static/chunks/pages/index-f72cd372a596ceaa.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/jPho8fGOMPFrnmTiCIKzM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/118-3ce137c3ada2ee62.js","static/chunks/pages/index-9365ed8fd35ded81.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
